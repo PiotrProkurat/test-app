a1877828f0b7a26c7be3962f4f058d31fe33d450
Piotr Prokurat <piotrprokurat@gmail.com>
2023-03-09 20:26:16 +0100

    Added employee not found exception.

---
+++ src/main/java/pl/kurs/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package pl.kurs.exceptions;
+
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
